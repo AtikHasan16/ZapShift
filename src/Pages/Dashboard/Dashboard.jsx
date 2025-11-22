b156cda4fcf49b6b433a580be0f4a7362ce34c14
AtikHasan16 <akhasannaeem@gmail.com>
2025-11-22 13:51:35 +0600

    added all deliveries comp with table

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 const Dashboard = () => {
-
-  
-  return <>Dashboard</>;
+  return (
+    <>
+      <div className="urbanist">
+        <h1 className="text-4xl font-bold">Dashboard</h1>
+        <p className="font-semibold">
+          You can access all your data and information from anywhere.{" "}
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/Pages/Dashboard/Deliveries.jsx
+++ src/Pages/Dashboard/Deliveries.jsx
@@ -1,7 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import useAuth from "../../Hooks/useAuth";
+import useAxios from "../../Hooks/useAxios";
 
 const Deliveries = () => {
-  return <div>Deliveries</div>;
+  const { user } = useAuth();
+  const axiosSecure = useAxios();
+
+  return (
+    <div className="urbanist">
+      <h1 className="text-4xl font-bold">All Deliveries</h1>
+
+      <div className="grid grid-cols-3 gap-6 my-6">
+        <div className="bg-white rounded-2xl p-6">
+          <p>Total</p>
+          <h1>{}</h1>
+        </div>
+        <div className="bg-white rounded-2xl p-6">
+          <p>Return</p>
+          <h1>{}</h1>
+        </div>
+        <div className="bg-white rounded-2xl p-6">
+          <p>Paid Return</p>
+          <h1>{}</h1>
+        </div>
+      </div>
+      <div className="my-10">
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Cons. ID</th>
+                <th>Parcel Name</th>
+                <th>Receiver Info</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>
+                  <button className="bg-blue-400 text-white p-1 rounded">
+                    View
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Deliveries;
